accc218714b0767a87cd8d898c3d4c8f20a2dd60
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-09-30 17:24:33 +0200

    ejercicios poo tema 11 polimorfismo

---
+++ servidor/t11/carrito-polimorfismo/Carrito.php
@@ -0,0 +1,26 @@
+<?php
+
+class Carrito
+{
+    private $productos = [];
+
+    public function meter($producto){
+        $this->productos[] = $producto;
+    }
+
+    public function mostrar(){
+        $total = 0;
+        $totaIva = 0;
+        echo "<div class='carrito'>";
+        foreach ($this->productos as $producto) {
+            echo "<article class='lineacarrito'>";
+            echo $producto->mostrar();
+            echo "</article>";
+            $total += $producto->precio();
+            $totaIva += $producto->precioIva();
+        }
+        echo "<div class='totalcarrito'>Total: $total Total con IVA: </div>";
+        echo "</div>";
+    }
+
+}
